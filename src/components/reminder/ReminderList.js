03934be346fae38e9da36cb493958b55ff473af4
Tommy Spurlock <tspurloc@gmail.com>
2019-04-03 16:00:43 -0400

    working post and delete. working on edit. need to get id to state

--- src/components/reminder/ReminderList.js
+++ src/components/reminder/ReminderList.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
+import ModalForm from './ModalForm'
+import ModalEditForm from './ModalEditForm'
 
 export default class ReminderList extends Component {
+  state = {
+  id: "",
+  name: "",
+  birthdate: "",
+  email: "",
+  phoneNumber: "",
+  notes: "",
+  imageURL: "",
+  userId: parseInt(sessionStorage.getItem("credentials"))
+}
+
+
+
+
   render() {
     return (
       <React.Fragment>
-          <button data-target="modal1" class="btn modal-trigger">Modal</button>
+          <button data-target="modal1" className="btn modal-trigger">Add Reminder</button>
 
         {this.props.reminders.map(reminder =>
         <div key={reminder.id}className="container">
@@ -35,14 +51,40 @@ export default class ReminderList extends Component {
                    {reminder.notes}
                 </p>
               </div>
-              <div className="card-action">
-                <a href="#">This is a link</a>
+              <div className="row">
+              <div className="card-action col s6">
+                <button className="btn" onClick={() => this.props.deleteReminder(reminder.id)}>Delete Reminder</button>
+              </div>
+              <div className="card-action col s6">
+                <button id={reminder.id} data-target="modal2" className="btn modal-trigger" onClick={() => {
+                      const newstate = {
+                          id: reminder.id,
+                          name: reminder.name,
+                          birthdate: reminder.birthdate,
+                          email: reminder.email,
+                          phoneNumber: reminder.phoneNumber,
+                          notes: reminder.notes,
+                          imageURL: reminder.imageURL,
+                          userId: parseInt(sessionStorage.getItem("credentials"))}
+                      this.setState(newstate)}
+                }
+                      >Edit Reminder</button>
+              </div>
               </div>
             </div>
           </div>
         </div>
         </div>
+
         )}
+        <div>
+          <ModalForm  addreminder={this.props.addReminder}/>
+        </div>
+        <div>
+          <ModalEditForm
+          name={this.state.name}
+          updateReminder={this.props.updateReminder}/>
+        </div>
       </React.Fragment>
     );
   }
